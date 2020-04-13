de08a71d800ba705c2ed9a1bed275a44fc0a76a1
1113896152 <57804975+1113896152@users.noreply.github.com>
2020-04-13 22:51:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def query_room(roomname):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config["REDIS_URL"] = "redis://127.0.0.1:6379/0"
-app.config["SQLALCHEMY_DATABASE_URI"]='mysql://root:l.1322630122@localhost:3306/first_flask?charset=utf8'
+app.config["SQLALCHEMY_DATABASE_URI"]='mysql://root:yourmysqlpassword@localhost:3306/first_flask?charset=utf8'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]='False'
 db=SQLAlchemy(app)
 socketio = SocketIO(app)
@@ -213,4 +213,4 @@ def test_disconnect():
             emit('people_num', r.count,room=session['room'])
 
 if __name__ == '__main__':
-    socketio.run(app,debug=True)
+    socketio.run(app)
